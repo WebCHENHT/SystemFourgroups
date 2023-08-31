a9b57cff0faf3ff94d3a69469badf8453ae7d7f6
chenjingao <1927256022@qq.com>
2023-08-31 19:40:33 +0800

    表格CSS样式

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,15 @@ import store from './stores'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
+//element国际化
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 // element样式
 import 'element-plus/dist/index.css'
 // iconfont
 import '@/assets/icon/iconfont.css'
 import '@/assets/icon/exam/iconfont-exam.css'
-import './assets/main.css'
 
+import './assets/reset.css'
 const app = createApp(App)
 app.use(ElementPlus, {
   locale: zhCn
