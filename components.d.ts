d148e6d087b1c3224f9d5ac9d70f949e5f196c7a
‘sujiahao’ <wei58870hu11100@126.com>
2023-08-31 18:47:19 +0800

    测试

--- components.d.ts
+++ components.d.ts
@@ -8,10 +8,13 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     EcharTs: typeof import('./src/components/EcharTs.vue')['default']
+    ElAside: typeof import('element-plus/es')['ElAside']
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
+    ElMain: typeof import('element-plus/es')['ElMain']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TableangPage: typeof import('./src/components/TableangPage.vue')['default']
