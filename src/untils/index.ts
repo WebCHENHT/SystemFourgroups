cdbfb9c88f3dfddbbd6c247335f56195c2a8fcc2
chenjingao <1927256022@qq.com>
2023-08-31 20:38:11 +0800

    考试

--- src/untils/index.ts
+++ src/untils/index.ts
@@ -20,7 +20,6 @@ export default function () {
       return item.url !== null
     })
     res.forEach((item: any) => {
-      console.log(item.url)
       const viewsurl =
         '../views' +
         item.url +
@@ -29,7 +28,6 @@ export default function () {
         String(item.url).slice(2) +
         'View' +
         '.vue'
-      console.log(viewsurl)
 
       const obj = {
         path: '/SystemMenu' + item.url,
